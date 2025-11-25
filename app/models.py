f6d6d0f4ff5836f648a7cabb3f18dca849b6398f
HodaJahangiri <hoda.cs87@gmail.com>
2025-11-25 10:26:58 -0800

    Create Parts and PartDescriptions models, ERD for new models, and create tables.

--- app/models.py
+++ app/models.py
@@ -9,8 +9,8 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class = Base)
 
 
-ticket_mechanic = Table(
-    "ticket_mechanic",
+ticket_mechanics = Table(
+    "ticket_mechanics",
     Base.metadata,
     Column("service_ticket_id", Integer, ForeignKey("service_tickets.id"),nullable=False),
     Column("mechanic_id", Integer, ForeignKey("mechanics.id"),nullable=False)
@@ -26,7 +26,7 @@ class Customers(Base):
     password : Mapped[str] = mapped_column(String(200), nullable=False)
     phone : Mapped[str] = mapped_column(String(50), nullable=False)
     address : Mapped[str] = mapped_column(String(500), nullable=True)
-    # relationship with service_tickets
+    # Relationship with service_tickets
     service_tickets : Mapped[list["Service_tickets"]] = relationship("Service_tickets", back_populates="customer")
 
 class Service_tickets(Base):
@@ -37,10 +37,12 @@ class Service_tickets(Base):
     service_desc : Mapped[str] = mapped_column(String(400), nullable=True)
     price : Mapped[float] = mapped_column(Float, nullable=False)
     VIN : Mapped[str] = mapped_column(String(100), nullable=False)
-    # relationship with customer
+    # Relationship with customer
     customer : Mapped["Customers"] = relationship("Customers", back_populates="service_tickets")
-    # relationship with mechanics
-    mechanics : Mapped[list["Mechanics"]] = relationship("Mechanics", secondary= ticket_mechanic, back_populates="tickets")
+    # Relationship with mechanics
+    mechanics : Mapped[list["Mechanics"]] = relationship("Mechanics", secondary= ticket_mechanics, back_populates="tickets")
+    # Relationship with parts
+    parts : Mapped[list["Parts"]] = relationship("Parts", back_populates="service_ticket")
 
 
 class Mechanics(Base):
@@ -54,5 +56,28 @@ class Mechanics(Base):
     phone : Mapped[str] = mapped_column(String(50), nullable=False)
     address : Mapped[str] = mapped_column(String(500), nullable=True)
     salary : Mapped[float] = mapped_column(Float, nullable=False)
-    # relationship with service_tickets
-    tickets : Mapped[list["Service_tickets"]] = relationship("Service_tickets", secondary=ticket_mechanic, back_populates="mechanics")
+    # Relationship with service_tickets
+    tickets : Mapped[list["Service_tickets"]] = relationship("Service_tickets", secondary=ticket_mechanics, back_populates="mechanics")
+
+
+class Parts(Base):
+    __tablename__ = "parts"
+
+    id : Mapped[int] = mapped_column(primary_key=True)
+    ticket_id : Mapped[int] = mapped_column(Integer, ForeignKey('service_tickets.id'), nullable=True)
+    desc_id : Mapped[int] = mapped_column(Integer, ForeignKey('part_descriptions.id'),nullable=False)
+    serial_number : Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    # Relationship with service_tickets
+    service_ticket : Mapped["Service_tickets"] = relationship("Service_tickets", back_populates="parts")
+    # Relationship with pert_descriptions
+    part_description : Mapped["PartDescriptions"] = relationship("PartDescriptions", back_populates="parts")
+
+class PartDescriptions(Base):
+    __tablename__ = "part_descriptions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(225), nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    made_in : Mapped[str] = mapped_column(String(200), nullable=False)
+    # Relationship with parts
+    parts : Mapped[list["Parts"]] = relationship("Parts", back_populates="part_description")
