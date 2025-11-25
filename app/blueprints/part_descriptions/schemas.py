8a7663236f4c6168a463040f40c078cb77d7621c
HodaJahangiri <hoda.cs87@gmail.com>
2025-11-25 14:03:39 -0800

    Create a rout to Create part or parts in Part table, and tested.

--- app/blueprints/part_descriptions/schemas.py
+++ app/blueprints/part_descriptions/schemas.py
@@ -5,7 +5,6 @@ from app.extensions import ma
 class PartDescriptionSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = PartDescriptions
-        include_fk = True
 
 part_description_schema = PartDescriptionSchema()
 part_descriptions_schema = PartDescriptionSchema(many=True)
--- app/blueprints/parts/routes.py
+++ app/blueprints/parts/routes.py
@@ -1,3 +1,45 @@
+from . import parts_bp
+from .schemas import part_schema, parts_schema
+from flask import request, jsonify
+from marshmallow import ValidationError
+from app.models import db, Parts, PartDescriptions
+
+
+
+@parts_bp.route('', methods=['POST'])
+def create_part():
+    try:
+        data = part_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify({"error": e.messages}), 400
+    quantity = request.args.get('gty', 1, type=int)
+    count = 0
+    base_serial = data["serial_number"]
+    new_serial = base_serial
+    serial = 1 
+    while count < quantity:
+        while check_existing_serial_number(new_serial):
+            new_serial = base_serial + "-" + str(serial)
+            serial += 1
+        data['serial_number'] = new_serial
+        new_part = Parts(**data)
+        db.session.add(new_part)
+        count += 1
+    db.session.commit()
+    return jsonify(f"Successfully created {quantity} part(s) with description id: {data["desc_id"]}(s)."), 200
+
+
+def check_existing_serial_number(serial_number):
+    existing_part = db.session.query(Parts).where(Parts.serial_number==serial_number).first()
+    if existing_part:
+        return True
+    return False
+
+
+
+
+
+
 
 
 
