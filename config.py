161d5cd12ace890841125610379afebe71df8be0
HodaJahangiri <hoda.cs87@gmail.com>
2025-12-02 08:37:37 -0800

    Create a tests folder and empty test_mechanics.py file for test mechanics routes, add config for testing.

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ class DevelopmentConfig:
     CACHE_DEFAULT_TIMEOUT = 200
     
 
-
-
 class TestingConfig:
-    pass
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing_mechanic_app.db'
+    DEBUG = True
+    CACHE_TYPE = "SimpleCache"
+    CACHE_DEFAULT_TIMEOUT = 300
+    TESTING = True
 
 
 class ProductionConfig:
