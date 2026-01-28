ab1b39369a82e76e0ff81568e8847bb4b0106e25
HodaJahangiri <hoda.cs87@gmail.com>
2026-01-28 11:51:38 -0800

    Change the default limitation for calling apis.

--- app/extensions.py
+++ app/extensions.py
@@ -7,7 +7,7 @@ ma = Marshmallow()
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["500 per day", "100 per hour"]
+    default_limits=["1000 per day", "500 per hour"]
 )
 
 cache = Cache()
